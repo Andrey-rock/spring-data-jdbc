1fecc4edfced698d70f8d75d6f5e153d0b6ffd53
Andrey-rock <andrey-rok@maol.ru>
2025-09-17 23:30:03 +0300

    Выполнено

---
+++ src/main/java/com/example/spring_data_jdbc/BookService.java
@@ -0,0 +1,48 @@
+package com.example.spring_data_jdbc;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Optional<Book> getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Optional<Book> updateBook(Long id, Book bookDetails) {
+        Optional<Book> existingBook = bookRepository.findById(id);
+        if (existingBook.isPresent()) {
+            Book book = existingBook.get();
+            book.setTitle(bookDetails.getTitle());
+            book.setAuthor(bookDetails.getAuthor());
+            book.setPublicationYear(bookDetails.getPublicationYear());
+            return Optional.of(bookRepository.save(book));
+        }
+        return Optional.empty();
+    }
+
+    public boolean deleteBook(Long id) {
+        if (bookRepository.existsById(id)) {
+            bookRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/com/example/spring_data_jdbc/SpringDataJdbcApplication.java
+++ src/main/java/com/example/spring_data_jdbc/SpringDataJdbcApplication.java
@@ -1,8 +1,10 @@
 package com.example.spring_data_jdbc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition
 @SpringBootApplication
 public class SpringDataJdbcApplication {
 
